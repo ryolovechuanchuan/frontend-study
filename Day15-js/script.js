41be6b9621e6d49a54509ef74b3282be4c7aa03c
Ryo <book61717@gmail.com>
2026-06-30 09:50:35 +0800

    Day15 JavaScript Variables

---
+++ Day15-js/script.js
@@ -0,0 +1,11 @@
+const name = "Ryo";
+const age = "30";
+const country = "Taiwan"
+const goal = "Frontend Engineer in Japan";
+const isLearning = true;
+
+console.log(name);
+console.log(age);
+console.log(country);
+console.log(goal);
+console.log(isLearning);
